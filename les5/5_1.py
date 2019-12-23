bc3c2f4a052b827d0404f19d39c878fb6454bf24
Andrey.Balandin <Andrey.Balandin@megafon.ru>
2019-12-23 21:50:44 +0300

    'Задание5'

---
+++ les5/5_1.py
@@ -0,0 +1,13 @@
+file = open('1.txt', 'w')
+line = input('Введите текст \n')
+while line:
+    file.writelines(line)
+    line = input('Введите текст \n')
+    if not line:
+       break
+
+file.close()
+file = open('1.txt', 'r')
+content = file.readlines()
+print(content)
+file.close()
